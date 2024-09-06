21169a1d94506395d1edfb5ac822cd747d917031
NickKuhns28 <nk985323@ohio.edu>
2024-09-05 20:36:45 -0400

    Update git-lab-program.cc

--- git-lab-program.cc
+++ git-lab-program.cc
@@ -1,1 +1,18 @@
-# git-lab-2
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nick Kuhns>
+ *        Date: <Sept 5, 2024>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+   cout << "Hello Git!!" << endl;
+    return 0;
+}// main
